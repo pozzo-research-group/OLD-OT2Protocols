1b47aea281d2c9087429271bf4b3c1f4fc710dc6
pozzo-group-robots <pozzorg@uw.edu>
2019-11-01 15:38:14 -0700

    edits to notebook and py file to ensure the code worked. Final code run in the OT2 jupyter notebook

--- 1000pCalibrationSpeed.py
+++ 1000pCalibrationSpeed.py
@@ -2,10 +2,11 @@ from opentrons import labware, instruments, robot
 robot.reset()
 robot.home()
 
-trash = robot.fixed_trash
-tiprack_1000 = labware.load("tiprack-1000ul", '4')
-stock = labware.load("vial-20ml", '2')
+#load labware
+tiprack_1000 = labware.load("tiprack-1000ul", '1')
+Stock1 = labware.load("vial-20ml", '2')
 vials = labware.load("vial-20ml", '3') #More scintillation vials
+trash = robot.fixed_trash
 
 P1000 = instruments.P1000_Single(
     mount = 'right',
@@ -13,21 +14,26 @@ P1000 = instruments.P1000_Single(
     trash_container = trash
 )
 
+robot.home()
+
+P1000.pick_up_tip()
 
 speeds = [.2,.4,.6,.8,1]
 pipette_amount = [600]
+P1000.aspirate(Stock1('A1'))
+P1000.dispense(Stock1('A1'))
+P1000.aspirate(Stock1('A1'))
+P1000.dispense(Stock1('A1'))
 
-for amount in pipette_amount:
-    P1000.aspirate(Stock1(source))
-    P1000.dispense(Stock1(source))
-    P1000.aspirate(Stock1(source))
-    P1000.dispense(Stock1(source))
-    for speed in speeds:
-        # distribution of the same amount at various speeds
-        P1000.distribute(
-            amount,
-            Stock1(source),
-            vials(counter),
-            rate = speed,
-            new_tip = "never")
-        print(pipette_amount, rate)
+for counter, speed in enumerate(speeds, 0):
+    # distribution of the same amount at various speeds
+    P1000.distribute(
+        pipette_amount,
+        Stock1('A1'),
+        vials(counter),
+        rate = speed,
+        new_tip = "never")
+    P1000.blow_out()
+    #print(pipette_amount, rate)
+
+P1000.drop_tip()
